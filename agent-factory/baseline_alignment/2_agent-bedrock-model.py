57678aaa433b842400f4c71d17b0da5cce7fc570
hRJ06 <hindolroy.2306@gmail.com>
2026-03-04 01:16:15 +0530

    Add Bedrock Strand Agent

---
+++ agent-factory/baseline_alignment/2_agent-bedrock-model.py
@@ -0,0 +1,14 @@
+from strands import Agent
+from strands.models import BedrockModel
+
+model = BedrockModel(
+    model_id="us.anthropic.claude-3-haiku-20240307-v1:0",  
+    temperature=0.3,                          
+
+    top_p=0.8,
+
+    streaming=True
+)
+
+agennt = Agent(model=model)
+response = agennt("What is AWS re:Invent? Answer in one sentence.")
